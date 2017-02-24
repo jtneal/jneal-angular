2189fa539573a500fb23e2101bc353695b88100a
Jason Neal <jtneal@gmail.com>
2017-02-23 23:11:15 -0500

    Tie in real service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule }                 from '@angular/core';
 import { BrowserModule }            from '@angular/platform-browser';
+import { HttpModule }               from '@angular/http';
+
+import { AppRoutingModule }         from './app-routing.module';
 
 import { AppComponent }             from './app.component';
 import { HomeComponent }            from './home.component';
@@ -7,11 +10,10 @@ import { PortfolioDetailComponent } from './portfolio-detail.component';
 import { PortfoliosComponent }      from './portfolios.component';
 import { PortfolioService }         from './portfolio.service';
 
-import { AppRoutingModule }         from './app-routing.module';
-
 @NgModule({
   imports: [
     BrowserModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
--- src/app/portfolio.service.ts
+++ src/app/portfolio.service.ts
@@ -1,16 +1,33 @@
-import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
 
-import { Portfolio }  from './portfolio';
-import { PORTFOLIOS } from './mock-portfolios';
+import { Injectable }    from '@angular/core';
+import { Http }          from '@angular/http';
+
+import { Portfolio }     from './portfolio';
 
 @Injectable()
 export class PortfolioService {
+  private portfoliosUrl = 'http://jneal.com/api/portfolio.php';
+
+  constructor(private http: Http) { }
+
   getPortfolios(): Promise<Portfolio[]> {
-    return Promise.resolve(PORTFOLIOS);
+    return this.http.get(this.portfoliosUrl)
+               .toPromise()
+               .then(response => response.json() as Portfolio[])
+               .catch(this.handleError);
   }
 
   getPortfolio(id: number): Promise<Portfolio> {
-    return this.getPortfolios()
-               .then(portfolios => portfolios.find(portfolio => portfolio.id === id));
+    const url = `${this.portfoliosUrl}?id=${id}`;
+    return this.http.get(url)
+               .toPromise()
+               .then(response => response.json() as Portfolio)
+               .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
