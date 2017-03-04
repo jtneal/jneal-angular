a2d6f292643afe16ee7b4499176309005983918f
Jason Neal <jtneal@gmail.com>
2017-03-03 19:27:36 -0500

    last minute adjustments for prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }     from '@angular/core';
+import { Router, NavigationEnd } from "@angular/router";
 
-import { Portfolio }         from './portfolio/shared/portfolio.model';
-import { PortfolioService }  from './portfolio/shared/portfolio.service';
+import { Portfolio }             from './portfolio/shared/portfolio.model';
+import { PortfolioService }      from './portfolio/shared/portfolio.service';
 
 @Component({
   moduleId: module.id,
@@ -12,10 +13,20 @@ import { PortfolioService }  from './portfolio/shared/portfolio.service';
 export class AppComponent implements OnInit {
   portfolioList: Portfolio[] = [];
 
-  constructor(private portfolioService: PortfolioService) {}
+  constructor(
+    private portfolioService: PortfolioService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getPortfolioList();
+
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
   }
 
   getPortfolioList(): void {
